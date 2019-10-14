ecb4ab123e868897a5f8eca1d06e94b18cbded50
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2019-10-14 11:58:18 +0100

    Decision Tree Regression - solution

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/regression_template.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/regression_template.py
@@ -24,12 +24,9 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state=0)
-regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = regressor.predict([[6.5]])
+y_pred = regressor.predict(6.5)
 
 # Visualising the Regression results
 plt.scatter(X, y, color = 'red')
